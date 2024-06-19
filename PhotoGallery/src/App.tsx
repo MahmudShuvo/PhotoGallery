6499684c0cd9783f26979f62dd311676ff8d0b75
MahmudShuvo <mhshuvo503948@gmail.com>
2024-06-19 19:29:26 +0600

    1st commit

---
+++ PhotoGallery/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+
+
+function App() {
+  
+
+  return (
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/signup" element={<SignUp/>}/>
+
+    
+   </Routes>
+  )
+}
+
+export default App
